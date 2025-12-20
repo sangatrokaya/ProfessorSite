a040d35736b553596d6dad67bab6f126fe7288e5
Sangat Rokaya <rokayasangat123@gmail.com>
2025-12-20 23:06:18 +0545

    Public layout created

--- frontend/src/layout/PublicLayout.jsx
+++ frontend/src/layout/PublicLayout.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const PublicLayout = () => {
-  return <div>PublicLayout</div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1 max-w-7xl mx-auto px-6 py-8">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default PublicLayout;
