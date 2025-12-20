06eaa80cf2a1b159e0aabb47af4e97d0f4bc135a
Sangat Rokaya <rokayasangat123@gmail.com>
2025-12-20 22:59:04 +0545

    placeholder public pages created and routes are wired in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* --------- LAYOUTS -------------- */
+import PublicLayout from "./layout/PublicLayout.jsx";
+import AdminLayout from "./layout/AdminLayout.jsx";
+
+/* ------------- PUBLIC PAGES ---------- */
+import Home from "./pages/public/Home.jsx";
+import About from "./pages/public/About.jsx";
+import Papers from "./pages/admin/Papers.jsx";
+import Courses from "./pages/public/Courses.jsx";
+import Blogs from "./pages/admin/Blogs.jsx";
+import Videos from "./pages/admin/Videos.jsx";
+import Contact from "./pages/public/Contact.jsx";
+
+/* ----------- ADMIN PAGES -------------- */
 import AdminLogin from "./pages/admin/AdminLogin.jsx";
 import Dashboard from "./pages/admin/Dashboard.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
-import AdminLayout from "./layout/AdminLayout.jsx";
-import Papers from "./pages/admin/Papers.jsx";
-import Courses from "./pages/admin/Courses.jsx";
-import Blogs from "./pages/admin/Blogs.jsx";
-import Videos from "./pages/admin/Videos.jsx";
+import AdminPapers from "./pages/admin/Papers.jsx";
+import AdminCourses from "./pages/admin/Courses.jsx";
+import AdminBlogs from "./pages/admin/Blogs.jsx";
+import AdminVideos from "./pages/admin/Videos.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public */}
+        {/* Public Section */}
+        <Route path="/" element={<PublicLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="papers" element={<Papers />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="videos" element={<Videos />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+
+        {/* Admin Section upto where Public can access */}
         <Route path="/admin/login" element={<AdminLogin />} />
 
         {/* Protected Admin Section */}
@@ -25,10 +50,10 @@ function App() {
           }
         >
           <Route path="dashboard" element={<Dashboard />} />
-          <Route path="papers" element={<Papers />} />
-          <Route path="courses" element={<Courses />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="videos" element={<Videos />} />
+          <Route path="papers" element={<AdminPapers />} />
+          <Route path="courses" element={<AdminCourses />} />
+          <Route path="blogs" element={<AdminBlogs />} />
+          <Route path="videos" element={<AdminVideos />} />
         </Route>
       </Routes>
     </BrowserRouter>
