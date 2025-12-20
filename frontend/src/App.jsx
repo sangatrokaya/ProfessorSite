b51c4fb15a8c170ce0337a9973866d7b77e79a0f
Sangat Rokaya <rokayasangat123@gmail.com>
2025-12-20 14:56:44 +0545

    admin layout created and used with nested routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLogin from "./pages/admin/AdminLogin.jsx";
 import Dashboard from "./pages/admin/Dashboard.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import AdminLayout from "./layout/AdminLayout.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public */}
         <Route path="/admin/login" element={<AdminLogin />} />
+
+        {/* Protected Admin Section */}
         <Route
-          path="/admin/dashboard"
+          path="/admin"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <AdminLayout />
             </ProtectedRoute>
           }
-        />
+        >
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/layout/AdminLayout.jsx
@@ -0,0 +1,33 @@
+import { Outlet } from "react-router-dom";
+
+const AdminLayout = () => {
+  return (
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <aside className="w-64 bg-slate-900 text-white p-4">
+        <h2 className="text-xl font-bold mb-6">Admin Panel</h2>
+        <nav className="space-y-3">
+          <p className="cursor-pointer">Dashboard</p>
+          <p className="cursor-pointer">Papers & Publications</p>
+          <p className="cursor-pointer">Courses</p>
+          <p className="cursor-pointer">Blogs</p>
+          <p className="cursor-pointer">Videos</p>
+        </nav>
+      </aside>
+
+      {/* Main */}
+      <main className="flex-1 bg-slate-100">
+        {/* Header */}
+        <header className="bg-white shadow px-6 py-4">
+          <h1 className="text-lg font-semibold">Dashboard</h1>
+        </header>
+
+        <section className="p-6">
+          <Outlet />
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default AdminLayout;
