bff9acc72b702a83694674baf3d4688642b83844
Sangat Rokaya <rokayasangat123@gmail.com>
2025-12-22 12:52:08 +0545

    Build Contact controller API with nodemailer

---
+++ backend/controllers/contactController.js
@@ -0,0 +1,44 @@
+import nodemailer from "nodemailer";
+
+// @desc Send contact message email
+// @route POST /api/contact
+// @aceess Public
+export const sendContactMessage = async (req, res) => {
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    return res.status(400).json({ message: "All fields are required!" });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: `"Processor Website" <${process.env.EMAIL_USER}>`,
+      to: process.env.EMAIL_RECEIVER,
+      replyTo: email,
+      subject: "New Contact Message",
+      html: `
+        <h3>New Contact Message</h3>
+        <p><strong>Name:</strong>${name}</p>
+        <p><strong>Email:</strong>${email}</p>
+        <p><strong>Message:</strong></p>
+        <p>${message}</p>
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.json({ message: "Message sent successfully!" });
+  } catch (error) {
+    console.error("Email send error: ", error);
+    res.status(500).json({ message: "Failed to send message!" });
+  }
+};
