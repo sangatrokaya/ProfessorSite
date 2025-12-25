6ea0481aba884098a303d3866e58f51486715eb8
Sangat Rokaya <rokayasangat123@gmail.com>
2025-12-25 22:28:47 +0545

    Minor logical changes in Profile controller APIs

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,28 +1,42 @@
 import Profile from "../models/Profile.js";
 
-// @desc Get public profile
+// @desc Get public professor profile
 // @route GET /api/profile
 // @access Public
-export const getProfile = async (req, res) => {
-  const profile = await Profile.findOne();
-  res.json(profile);
+export const getPublicProfile = async (req, res) => {
+  try {
+    const profile = await Profile.findOne();
+
+    if (!profile) {
+      return res.status(404).json({ message: "Profile not created yet!" });
+    }
+    res.json(profile);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch  profile!" });
+  }
 };
 
-// @desc Create or update profile
+// @desc Create or update professor profile (singleton)
 // @route PUT /api/profile
 // @access Admin
 export const upsertProfile = async (req, res) => {
-  const profile = await Profile.findOne();
+  try {
+    const existingProfile = await Profile.findOne();
 
-  if (profile) {
-    const updatedProfile = await Profile.findByIdAndUpdate(
-      profile._id,
-      req.body,
-      { new: true }
-    );
-    res.json(updatedProfile);
-  } else {
-    const createdProfile = await Profile.create(req.body);
-    res.status(201).json(createdProfile);
+    if (existingProfile) {
+      const updatedProfile = await Profile.findByIdAndUpdate(
+        existingProfile._id,
+        req.body,
+        { new: true }
+      );
+      return res.json(updatedProfile);
+    }
+
+    const newProfile = await Profile.create(req.body);
+    res.status(201).json(newProfile);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Profile update failed!", error: error.message });
   }
 };
