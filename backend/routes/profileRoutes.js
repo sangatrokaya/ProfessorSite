ada8e4c8a7bdde17538ffcbf531ff939d0c2a8bc
Sangat Rokaya <rokayasangat123@gmail.com>
2025-12-25 22:30:24 +0545

    Minor changes into Profile routes

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getProfile, upsertProfile } from "../controllers/profileController.js";
+import {
+  getPublicProfile,
+  upsertProfile,
+} from "../controllers/profileController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getProfile);
+router.get("/", getPublicProfile);
 router.put("/", protect, upsertProfile);
 
 export default router;
