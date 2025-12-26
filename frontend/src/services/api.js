2a31e98489e5168dc8168933b897be76e2a61060
Sangat Rokaya <rokayasangat123@gmail.com>
2025-12-26 12:43:54 +0545

    Centralize Axios auth in api services

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
+import { store } from "@/store";
 
 const api = axios.create({
-  baseURL: `http://localhost:4000`,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+});
+
+api.interceptors.request.use((config) => {
+  const { adminInfo } = store.getState().auth;
+
+  if (adminInfo?.token) {
+    config.headers.Authorization = `Bearer ${adminInfo.token}`;
+  }
+  return config;
 });
 
 export default api;
